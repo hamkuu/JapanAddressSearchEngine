f98dafc07a98e6f01dbd177289dd35eebc618635
CRH Shaw <k.shou@creativehope.co.jp>
2019-12-04 16:41:34 +0900

    :bug: fix index as starting with zero

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -3,9 +3,9 @@ require "json"
 
 japan_address_hash = {}
 
-csv_file = CSV.open("KEN_ALL.CSV", headers: false, encoding: "Shift_JIS:UTF-8")
+csv_file = CSV.open("data/KEN_ALL.CSV", headers: false, encoding: "Shift_JIS:UTF-8")
 csv_file.each do |csv_row|
-  japan_address_hash.store(csv_file.lineno, csv_row)
+  japan_address_hash.store(csv_file.lineno - 1, csv_row)
 end
 
 File.open("data/ken_all.json", "w") do |f|
