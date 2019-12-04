90a5192a83e8d15536deac39a7e9ef5fa75d521f
CRH Shaw <k.shou@creativehope.co.jp>
2019-12-04 17:30:47 +0900

    :recycle: encapsulate main process as AddressSearchEngine

---
+++ address_search_engine.rb
@@ -0,0 +1,30 @@
+require "json"
+require "set"
+
+class AddressSearchEngine
+  def start
+    japan_addresses = JSON.parse(File.read("data/ken_all.json"))
+
+    bigram_map = JSON.parse(File.read("data/bigram_map.json"))
+
+    loop do
+      puts "Input two Kanji for address query:"
+      input_text = gets
+      query_text = input_text.strip
+
+      break if query_text == "exit"
+
+      if query_text.length != 2
+        puts "expect only two Kanji"
+        next
+      end
+
+      for row in bigram_map[query_text]
+        puts japan_addresses[row.to_s].inspect
+      end
+    end
+  end
+end
+
+myAddressSearchEngine = AddressSearchEngine.new
+myAddressSearchEngine.start
